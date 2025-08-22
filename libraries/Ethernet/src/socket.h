ec296f216be46c868fc84a04491e43116836455a
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-08-22 11:37:51 -0700

    performed cleanup

---
+++ libraries/Ethernet/src/socket.h
@@ -0,0 +1,2 @@
+// Forwarding header so Arduino can find <Ethernet/socket.h>
+#include "../socket.h"
---
+++ libraries/Ethernet/src/wizchip_conf.h
@@ -0,0 +1,2 @@
+// Forwarding header so Arduino can find <Ethernet/wizchip_conf.h>
+#include "../wizchip_conf.h"
