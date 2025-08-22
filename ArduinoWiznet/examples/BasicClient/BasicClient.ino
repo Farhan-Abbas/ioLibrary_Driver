56fb0442cf7c1b0f3a53653815e2822f29b5e7a8
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-08-22 10:45:41 -0700

    Refactor ArduinoWiznet: Remove old implementation and introduce EthernetClass and EthernetUDP for improved functionality

--- ArduinoWiznet/examples/BasicClient/BasicClient.ino
+++ ArduinoWiznet/examples/BasicClient/BasicClient.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-#include "ArduinoWiznet.h"
+#include "Ethernet_Arduino.h"
 
 // Define CS control for WIZnet module (change pin as required)
 const int CS_PIN = 10;
@@ -28,24 +28,33 @@ void setup() {
   Serial.begin(115200);
   SPI.begin();
   pinMode(CS_PIN, OUTPUT);
-  cs_deselect_cb();
+  digitalWrite(CS_PIN, HIGH);
 
-  ArduinoWiznet::begin(spi_readbyte_cb, spi_writebyte_cb, cs_select_cb, cs_deselect_cb);
+  // Use the Arduino wrapper EthernetClass
+  EthernetClass eth(CS_PIN, SPI);
+  IPAddress ipaddr(192,168,1,100), gw(192,168,1,1), sn(255,255,255,0), dns(8,8,8,8);
+  eth.config(ipaddr, gw, sn, dns);
+  eth.setSPISpeed(36000000);
+  if(eth.begin() != 0){
+    Serial.println("wizchip init failed");
+    while(1);
+  }
 
-  ArduinoWiznet::SocketClient client(0);
-  if(client.beginClient(0) != 0){
+  // use raw socket for simple TCP GET via socket API
+  int8_t s = socket(0, Sn_MR_TCP, 0, 0);
+  if(s != 0){
     Serial.println("socket open failed");
     while(1);
   }
 
-  uint8_t ip[4] = {192,168,1,100}; // change to target
-  if(client.connectIP(ip, 80) == 0){
+  uint8_t dest[4] = {192,168,1,100};
+  if(connect(0, dest, 80) == SOCK_OK){
     const char* msg = "GET / HTTP/1.0\r\nHost: 192.168.1.100\r\n\r\n";
-    client.send((const uint8_t*)msg, strlen(msg));
+    send(0, (uint8_t*)msg, strlen(msg));
     uint8_t buf[256];
-    int r = client.receive(buf, sizeof(buf));
+    int32_t r = recv(0, buf, sizeof(buf));
     if(r > 0) Serial.write(buf, r);
-    client.close();
+    close(0);
   } else {
     Serial.println("connect failed");
   }
